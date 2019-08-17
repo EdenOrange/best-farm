49beef26ebdebad3cf738079727de1014bae1b2a
Nicholas Thie <ntnicholasthie@gmail.com>
2019-08-17 23:11:14 +0700

    Added mobile navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,67 @@
 import React from 'react';
-import './Navbar.css';
+import './Navbar.scss';
+import logo from '../../assets/images/logo.svg';
+
+function NavbarLogo() {
+  return (
+    <img src={logo} className='navbar-logo' />
+  );
+}
+
+class NavbarMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuList: [
+        {
+          name: 'Home',
+          href: '#'
+        },
+        {
+          name: 'Gallery',
+          href: '#'
+        },
+        {
+          name: 'Contact Us',
+          href: '#'
+        }
+      ],
+      menuToggled: false
+    }
+    this.openMenu = this.openMenu.bind(this);
+  }
+
+  openMenu(event) {
+    this.setState({ menuToggled: !this.state.menuToggled });
+  }
+
+  render() {
+    const menus = this.state.menuList.map(menu => {
+      return (
+        <li><a href={menu.href}>{menu.name}</a></li>
+      );
+    });
+  
+    return (
+      <nav className='navbar-menu'>
+        <i className='fas fa-bars fa-lg' onClick={this.openMenu} />
+        {this.state.menuToggled && (
+          <ul>
+            {menus}
+          </ul>
+        )}
+      </nav>
+    );
+  }
+}
 
 class Navbar extends React.Component {
   render() {
     return (
-      <div>
-      </div>
+      <header className='navbar-container'>
+        <NavbarLogo />
+        <NavbarMenu />
+      </header>
     );
   }
 }
