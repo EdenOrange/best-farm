78c3e8b5ebc0dfb5710fcf69e8871ce651e8b7a6
Nicholas Thie <ntnicholasthie@gmail.com>
2019-08-17 21:33:46 +0700

    Folder restructuring

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../../assets/images/logo.svg';
+import './App.css';
+
+import Navbar from '../Navbar/Navbar';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Navbar />
+    );
+  }
+}
+
+export default App;
