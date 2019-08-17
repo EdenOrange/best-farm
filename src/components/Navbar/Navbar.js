8220e83d398a0e1b248103dab943b45714fb6246
Nicholas Thie <ntnicholasthie@gmail.com>
2019-08-17 23:46:15 +0700

    Added desktop navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import './Navbar.scss';
+import styles from './Navbar.scss';
 import logo from '../../assets/images/logo.svg';
+
+const MOBILE_WIDTH = parseInt(styles['mobile-width']);
 
 function NavbarLogo() {
   return (
-    <img src={logo} className='navbar-logo' />
+    <img src={logo} className='navbar-logo' alt='Best Farm logo' />
   );
 }
 
@@ -26,26 +28,38 @@ class NavbarMenu extends React.Component {
           href: '#'
         }
       ],
-      menuToggled: false
+      menuToggled: false,
+      mobileMenu: window.innerWidth < MOBILE_WIDTH
     }
     this.openMenu = this.openMenu.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.closeMenu.bind(this));
   }
 
   openMenu(event) {
     this.setState({ menuToggled: !this.state.menuToggled });
   }
 
+  closeMenu() {
+    this.setState({
+      menuToggled: this.state.menuToggled && window.innerWidth < MOBILE_WIDTH,
+      mobileMenu: window.innerWidth < MOBILE_WIDTH
+    });
+  }
+
   render() {
     const menus = this.state.menuList.map(menu => {
       return (
-        <li><a href={menu.href}>{menu.name}</a></li>
+        <li key={menu.name}><a href={menu.href}>{menu.name}</a></li>
       );
     });
   
     return (
       <nav className='navbar-menu'>
         <i className='fas fa-bars fa-lg' onClick={this.openMenu} />
-        {this.state.menuToggled && (
+        {(this.state.menuToggled || !this.state.mobileMenu) && (
           <ul>
             {menus}
           </ul>
