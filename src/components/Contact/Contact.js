88d826d76d3c73e077347053f1d1f4d780b72728
Nicholas Thie <ntnicholasthie@gmail.com>
2019-08-19 00:35:58 +0700

    Contact links open in new tab/window

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,14 +6,14 @@ class Contact extends React.Component {
     return (
       <section className='contact'>
         <h2>Hubungi Kami</h2>
-        <a href='https://api.whatsapp.com/send?phone=6281929541822&text=Halo, mau tanya-tanya tentang bestfarm dong' className='whatsapp'>
+        <a href='https://api.whatsapp.com/send?phone=6281929541822&text=Halo, mau tanya-tanya tentang bestfarm dong' className='whatsapp' target='_blank' rel='noopener noreferrer'>
           <i className='fab fa-whatsapp fa-2x'></i><span>0819 2954 1822</span>
         </a>
         <div className='media'>
-        <a href='https://www.facebook.com/bestfarm.bestfarm.7' className='facebook'>
+        <a href='https://www.facebook.com/bestfarm.bestfarm.7' className='facebook' target='_blank' rel='noopener noreferrer'>
           <i className='fab fa-facebook-f fa-2x'></i><span>bestfarm</span>
         </a>
-        <a href='https://www.instagram.com/bestfarmbelitung/' className='instagram'>
+        <a href='https://www.instagram.com/bestfarmbelitung/' className='instagram' target='_blank' rel='noopener noreferrer'>
           <i className='fab fa-instagram fa-2x'></i><span>bestfarmbelitung</span>
         </a>
         </div>
