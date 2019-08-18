a507c29a8175915b182f1ad94f30f29086c79726
Nicholas Thie <ntnicholasthie@gmail.com>
2019-08-18 11:58:24 +0700

    Added temp hero

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Hero.scss';
+
+class Hero extends React.Component {
+  render() {
+    return (
+      <div className='hero'>
+        
+      </div>
+    );
+  }
+}
+
+export default Hero;
