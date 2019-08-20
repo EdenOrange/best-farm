d9192a874b0b3e5e74d2dc249fd69875777e5ed8
Nicholas Thie <ntnicholasthie@gmail.com>
2019-08-20 15:28:12 +0700

    Updated location point

--- src/components/Location/Location.js
+++ src/components/Location/Location.js
@@ -9,7 +9,7 @@ class Location extends React.Component {
         <h4>Jl. Murai Dalam RT 34 RW 11</h4>
         <h5>Desa Aik Rayak</h5>
         <h5>Tanjung Pandan - Belitung</h5>
-        <iframe className='map' title='Best Farm Map Location' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13404.75253067676!2d107.67045826716893!3d-2.74005096469221!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMsKwNDQnMjAuNiJTIDEwN8KwNDAnMjQuOSJF!5e0!3m2!1sen!2sid!4v1566110898374!5m2!1sen!2sid" 
+        <iframe className='map' title='Best Farm Map Location' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22544.186219730254!2d107.68551566626928!3d-2.731034073037507!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e173dd40e51c9d3%3A0xa6727c034fa843a9!2sBestfarm+hydroponics!5e0!3m2!1sen!2sid!4v1566289545498!5m2!1sen!2sid" 
         frameBorder="0" allowFullScreen></iframe>
       </section>
     );
