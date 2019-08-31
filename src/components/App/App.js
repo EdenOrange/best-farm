8d41616bdbec9f9a15abd56eff3b43530b93c00d
Nicholas Thie <ntnicholasthie@gmail.com>
2019-08-31 14:48:16 +0700

    Added code splitting for route components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWhatsapp, faFacebookF, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { faBars, faTimes, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
-import Home from '../Home/Home';
-import Gallery from '../Gallery/Gallery';
-import Navbar from '../Navbar/Navbar';
+import Navbar from 'components/Navbar/Navbar';
+const Home = lazy(() => import('components/Home/Home'));
+const Gallery = lazy(() => import('components/Gallery/Gallery'));
 
 class App extends React.Component {
   constructor(props) {
@@ -28,18 +29,42 @@ class App extends React.Component {
     library.add(faWhatsapp, faFacebookF, faInstagram, faBars, faTimes, faSpinner);
   }
 
+  renderLoadingScreen() {
+    const containerStyle = {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: '100vw',
+      height: '100vh'
+    };
+
+    return (
+      <div style={containerStyle}>
+        <FontAwesomeIcon
+          icon='spinner'
+          className='icon'
+          size='3x'
+          spin
+        />
+      </div>
+    );
+  }
+
   render() {
     return (
       <Router>
         <Navbar />
-        <Route
-          path='/' exact
-          render={(props) => <Home {...props} menuList={this.state.menuList} />}
-        />
-        <Route
-          path='/gallery/' exact
-          render={(props) => <Gallery {...props} menuList={this.state.menuList} />}
-        />
+        <Suspense fallback={this.renderLoadingScreen()}>
+          <Route
+            path='/' exact
+            render={(props) => <Home {...props} menuList={this.state.menuList} />}
+          />
+          <Route
+            path='/gallery/' exact
+            render={(props) => <Gallery {...props} menuList={this.state.menuList} />}
+          />
+        </Suspense>
+        
       </Router>
     );
   }
